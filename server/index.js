dfba17c269f702173a50be4833b236ff7142642f
Zynkah <ffgurl07@yahoo.com>
2023-07-13 11:12:06 -0500

    first push

---
+++ server/index.js
@@ -0,0 +1,3 @@
+"use client";
+const http = require("http");
+
---
+++ src/app/data.js
@@ -0,0 +1,181 @@
+export default [
+    {
+        "id": "zVnCVxDmoy",
+        "timestamp": 1689198198000,
+        "user": {
+            "id": 48,
+            "image_url": "http://loremflickr.com/207/205/",
+            "name": "Shia LaBeouf",
+            "username": "lemke5988"
+        },
+        "message": "Encouraging most repelling these where swing dress walk occasionally scarcely this previously.",
+        "tags": [
+            "sustainable",
+            "taxidermy"
+        ]
+    },
+    {
+        "id": "xCpekrV",
+        "timestamp": 1689198196000,
+        "user": {
+            "id": 11448,
+            "image_url": "http://loremflickr.com/204/204/",
+            "name": "Zach Galifianakis",
+            "username": "paucek8370"
+        },
+        "message": "Shower it i.e. nature that themselves downstairs whoever us now it his.",
+        "tags": [
+            "photo booth",
+            "roof",
+            "keffiyeh",
+            "viral",
+            "try-hard",
+            "+1",
+            "kale chips",
+            "shoreditch"
+        ]
+    },
+    {
+        "id": "wCJjz",
+        "timestamp": 1689198194000,
+        "user": {
+            "id": 16195,
+            "image_url": "http://loremflickr.com/205/208/",
+            "name": "Jack Nicholson",
+            "username": "denesik5711"
+        },
+        "message": "This up that why none possess apartment most they of several panther.",
+        "tags": [
+            "3 wolf moon",
+            "retro",
+            "shabby chic",
+            "chambray",
+            "fixie",
+            "90's",
+            "mumblecore"
+        ]
+    },
+    {
+        "id": "bdsfxTKkc",
+        "timestamp": 1689198197000,
+        "user": {
+            "id": 12785,
+            "image_url": "http://loremflickr.com/208/200/",
+            "name": "Sandra Bullock",
+            "username": "moore5442"
+        },
+        "message": "Troop there win yearly boxers he summation usually why American here besides.",
+        "tags": [
+            "everyday",
+            "asymmetrical",
+            "vinegar"
+        ]
+    },
+    {
+        "id": "UCXhQb",
+        "timestamp": 1689198194000,
+        "user": {
+            "id": 16415,
+            "image_url": "http://loremflickr.com/205/208/",
+            "name": "Robin Williams",
+            "username": "hickle5380"
+        },
+        "message": "Our appetite next what for infrequently finally yours today within utterly ever.",
+        "tags": [
+            "photo booth",
+            "fashion axe",
+            "ugh",
+            "heirloom",
+            "pitchfork",
+            "mumblecore",
+            "listicle",
+            "irony",
+            "viral"
+        ]
+    },
+    {
+        "id": "aVzKhGQmmm",
+        "timestamp": 1689198194000,
+        "user": {
+            "id": 10847,
+            "image_url": "http://loremflickr.com/207/206/",
+            "name": "Owen Wilson",
+            "username": "mertz4191"
+        },
+        "message": "Furthermore someone research stupid before those moreover by either besides apple there.",
+        "tags": [
+            "ennui",
+            "brunch",
+            "cornhole",
+            "asymmetrical"
+        ]
+    },
+    {
+        "id": "mkYx",
+        "timestamp": 1689198194000,
+        "user": {
+            "id": 18627,
+            "image_url": "http://loremflickr.com/200/207/",
+            "name": "Heath Ledger",
+            "username": "gulgowski4138"
+        },
+        "message": "First she which nightly is religion nightly normally a this none why.",
+        "tags": [
+            "tacos",
+            "put a bird on it",
+            "everyday",
+            "before they sold out",
+            "tattooed"
+        ]
+    },
+    {
+        "id": "NZuiUhFI",
+        "timestamp": 1689198196000,
+        "user": {
+            "id": 14761,
+            "image_url": "http://loremflickr.com/205/209/",
+            "name": "Robert Downey Jr.",
+            "username": "west8876"
+        },
+        "message": "Advertising they speed let previously posse after her now pack back batch.",
+        "tags": [
+            "pug",
+            "cred",
+            "small batch",
+            "chicharrones",
+            "mixtape"
+        ]
+    },
+    {
+        "id": "flrvDvwQOF",
+        "timestamp": 1689198196000,
+        "user": {
+            "id": 14373,
+            "image_url": "http://loremflickr.com/208/207/",
+            "name": "Drew Barrymore",
+            "username": "weimann9138"
+        },
+        "message": "Team first been Iranian tax i.e. anything what account vase crew cry.",
+        "tags": [
+            "actually",
+            "tattooed",
+            "street"
+        ]
+    },
+    {
+        "id": "QubD",
+        "timestamp": 1689198195000,
+        "user": {
+            "id": 16974,
+            "image_url": "http://loremflickr.com/208/207/",
+            "name": "Brendan Fraser",
+            "username": "schmeler4019"
+        },
+        "message": "Have in why remain Atlantic being sail clarity one what year rarely.",
+        "tags": [
+            "leggings",
+            "selvage",
+            "irony"
+        ]
+    }
+]
